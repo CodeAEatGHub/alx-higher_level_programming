9721f195a24f24784ba070a9216020fd99425391
Abrham Endale <tmailae2020@gmail.com>
2022-09-22 13:16:24 -0700

    Fixed 2-args.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
+    if len(sys.argv) == 1:
+        print('1 argument:')
+    elif len(sys.argv) > 1:
+        print('{} arguments:'.format(len(sys.argv)))
     for i in range(len(sys.argv)):
         print('{}: {}'.format(i, sys.argv[i]))
