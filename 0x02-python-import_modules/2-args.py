1703e6be5b5b5bc80ac29cd90c7818326b906c9d
Abrham Endale <tmailae2020@gmail.com>
2022-09-23 09:58:40 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     import sys
     if len(sys.argv) - 1 == 0:
-        print('0 arguments:')
+        print('0 arguments.')
     if len(sys.argv) - 1 == 1:
         print('1 argument:')
     elif len(sys.argv) > 1:
