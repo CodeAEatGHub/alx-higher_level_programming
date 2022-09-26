697235e8c48f769f8f2bd6fd13999da322880746
Abrham Endale <tmailae2020@gmail.com>
2022-09-26 14:25:50 -0700

    Added palindrome task.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a list is palindrome
+ *
+ * @head: Address of pointer to the head of the list
+ *
+ * Return: 1 or 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t  *end_parser = *head, *front_parser = *head;
+	int i = 0, j = 0, *p;
+
+	if (head == NULL)
+		return (1);
+	while (end_parser != NULL)
+	{
+		i++;
+		if (end_parser->next == NULL)
+			break;
+		end_parser = end_parser->next;
+	}
+	p = malloc(sizeof(int) * i);
+	for (j = 0 ; j < i ; j++)
+	{
+		p[j] = front_parser->n;
+		front_parser = front_parser->next;
+	}
+	for (j = 0 ; j < i ; j++)
+	{
+		if (p[j] != p[i - 1 - j])
+			return (0);
+	}
+	free(p);
+	return (1);
+}
