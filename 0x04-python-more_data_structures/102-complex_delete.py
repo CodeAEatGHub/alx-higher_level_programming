42a55440d2cfcb4d056f9f42ba391e25d0dcbae1
Abrham Endale <tmailae2020@gmail.com>
2022-09-28 14:13:22 -0700

    Corrected complex delete code.

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary is None:
-        return None
     for i in list(a_dictionary):
         if a_dictionary[i] is value:
             del a_dictionary[i]
