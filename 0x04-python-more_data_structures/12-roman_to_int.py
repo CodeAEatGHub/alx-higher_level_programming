597c58c112671e1651420e698184068f7b506e48
Abrham Endale <tmailae2020@gmail.com>
2022-09-28 14:45:00 -0700

    Added Pybytes objects files.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return (0)
-    rom_num = [I, V, X, L, C, D, M]
+    rom_num = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
     num = [1, 5, 10, 50, 100, 500, 1000]
     j = 0
+    k = 0
+    r = []
+    if roman_string is None:
+        return (0)
     for i in rom_num:
         if roman_string == i:
-            return num[j]
+            r[k] =  num[j]
+            k = k + 1
         j = j + 1
+    for i in r:
+        if i > r[j + 1]:
+            op[j] = 1
+        else:
+            op[j] = 0
+        j = j + 1
+    for i in r:
+        if op[j + 1] is 1
+        sum = sum + 
     return (0)
