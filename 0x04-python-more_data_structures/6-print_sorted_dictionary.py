1519b93bb27291a2643da20c7855f46627799e7d
ABrham Endale <tmailae2020@gmail.com>
2022-09-28 20:42:08 +0000

    Fixed permission issue for task 7.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is None:
-        return None
-    for i in sorted(a_dictionary):
+    for i in sorted(a_dictionary.keys()):
         print('{} : {}'.format(i, a_dictionary[i]))
