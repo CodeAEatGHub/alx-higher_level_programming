1da6cc694033c571b8379e52a81294964c9034f1
Abrham Endale <tmailae2020@gmail.com>
2022-10-10 14:48:52 -0700

    Corrected erronous outputs.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    c = 0
-    exc = 0
-    try:
-        c = a / b
-        return c
-    except ZeroDivisionError:
-        exc = 1
-        return None
-    except ValueError:
-        exc = 1
-        return None
-    finally:
-        if not exc:
-            print('Inside result:{}'.format(c))
-        else:
-            print('Inside result: None')
+        c = 0
+        try:
+            c = a / b
+        except (TypeError, ZeroDivisionError):
+            c = None
+        finally:
+            print('Inside result: {}'.format(c))
+            return (c)
