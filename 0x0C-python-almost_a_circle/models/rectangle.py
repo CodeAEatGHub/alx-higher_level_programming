d55c373099bd789d1d3c36df3193096b565a5026
Abrham Endale <tmailae2020@gmail.com>
2022-10-31 09:50:43 -0700

    Added codes.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,6 +3,7 @@
 
 
 from models.base import Base
+import sys
 
 
 class Rectangle(Base):
@@ -17,15 +18,31 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes new instances."""
+        if type(width) not in [int, float]:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be > 0")
         self.__width = width
+        if type(height) not in [int, float]:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be > 0")
         self.__height = height
-        if type(id) not in [int, float]:
+        if type(x) not in [int, float]:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be > 0")
+        self.__x = x
+        if type(y) not in [int, float]:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be > 0")
+        self.__y = y
+        if id is not None and type(id) is not int:
             raise TypeError("id must be an integer")
-        if id < 0:
+        if id is not None and id < 0:
             raise ValueError("id must be > 0")
         Base.__init__(self, id)
-        self.x(x)
-        self.y(y)
 
     @property
     def width(self):
@@ -82,3 +99,50 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be > 0")
         self.__y = y
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return (self.__width * self.__height)
+
+    def display(self):
+        """Displays the rectangle using # characters."""
+        for i in range(0, self.__y):
+            print()
+        for j in range(0, self.__height):
+            for k in range(0, self.__x):
+                print(" ", end="")
+            for l in range(0, self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """Prints the string representation of the class."""
+        return f'[{Rectangle.__name__}] ({self.id}) {self.x}/{self.y} - {self.__width}/{self.__height}'
+
+    def update(self, *args, **kwargs):
+        """Updates the class attributes."""
+        if len(args) > 0:
+            print(args[0])
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
+        if 'width' in kwargs.keys():
+            self.__width = kwargs['width']
+        if 'height' in kwargs.keys():
+            self.__height = kwargs['height']
+        if 'x' in kwargs.keys():
+            self.__x = kwargs['x']
+        if 'y' in kwargs.keys():
+            self.__y = kwargs['y']
+        if 'id' in kwargs.keys():
+            self.id = kwargs['id']
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the instance."""
+        return (self.__dict__)
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes new square instances."""
+        Rectangle.__init__(self, size, size, x, y, id=None)
+
+    def __str__(self):
+        """Prints the string representation of the class."""
+        return f'[{Square.__name__}] ({self.id}) {self.x}/{self.y} - {self.height}'
+
+    @property
+    def size(self):
+        """Returns the value of size."""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """Sets the size of an instance."""
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """Updates the Square instance."""
+        b = ()
+        if len(args) > 1:
+            a = list(args)
+            a.insert(2, args[1])
+            b = tuple(a)
+            Rectangle.update(b)
+        Rectangle.update(args, kwargs)
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the instance."""
+        return (self.__dict__)
