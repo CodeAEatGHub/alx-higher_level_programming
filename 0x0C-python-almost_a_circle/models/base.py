8d824143573d748d158f486852631b52d2d10b73
Abrham Endale <tmailae2020@gmail.com>
2022-11-01 14:00:21 -0700

    Corrected save_to_file.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,7 +35,7 @@ class Base:
                 if i.__class__.__name__ == 'Rectangle':
                     d = {'width': i.width, 'height': i.height, 'x': i.x, 'y': i.y, 'id': i.id}
                 elif i.__class__.__name__ == 'Square':
-                    d = {'side': i.side, 'x': i.x, 'y': i.y, 'id': i.id}
+                    d = {'size': i.size, 'x': i.x, 'y': i.y, 'id': i.id}
                 a.append(json.loads(self.to_json_string(d)))
             name = list_objs[0].__class__.__name__ + ".json"
             with open(name, 'w', encoding="utf-8") as f:
