988b6d7900c6f8473fe0bdc8c6e5ee681e857c90
Abrham Endale <tmailae2020@gmail.com>
2023-01-17 14:16:04 -0800

    Fixed first task.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Checks status of web pages
 """
+Fetches a website URI with urllib
+"""
+import urllib.request
+
+
 if __name__ == "__main__":
-    import urllib.request
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        bytes_content = response.read()
-        content = bytes_content.decode('utf-8')
-        print_str = '''Body response:
-\t- type: {}
-\t- content: {}
-\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
-        print(print_str)
+    req = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+    print("Body response:\n"
+          "\t- type: {}\n"
+          "\t- content: {}\n"
+          "\t- utf8 content: {}".
+          format(type(html), html, html.decode('utf-8'))
+          )
