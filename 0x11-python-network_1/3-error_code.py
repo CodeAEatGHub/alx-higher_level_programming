86b0fdc8a137721940e0aa9ff0424d50c25d8f8d
Abrham Endale <tmailae2020@gmail.com>
2023-01-19 17:51:05 -0800

    Fixed some erronous outputs.

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as res:
             print(res.read().decode('utf-8'))
     except error.HTTPError as error:
-        print("Error code:", error.code)
+        print("Error code: ", error.code)
